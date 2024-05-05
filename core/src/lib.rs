2d9b2329e956ae02a7f10603b07fb978b35f3622
FaultyRAM <316014+FaultyRAM@users.noreply.github.com>
2024-05-06 02:13:00 +1000

    feat(core): calculate delta time on each `update`

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -5,6 +5,21 @@
 use bevy_ecs::prelude::*;
 use wasm_bindgen::prelude::*;
 
+#[derive(Clone, Copy, Debug, Resource)]
+/// Information about the current update interval.
+struct UpdateInterval {
+    /// The timestamp associated with the most recently executed update.
+    ///
+    /// Each time `Core::update` is called, before executing the update schedule, this is set to the
+    /// value of the caller-provided timestamp.
+    pub timestamp: f64,
+    /// The amount of time that has elapsed since the previous update.
+    ///
+    /// Each time `Core::update` is called, before executing the update schedule, this is set to the
+    /// difference of the caller-provided timestamp and the currently stored timestamp.
+    pub delta: f64,
+}
+
 #[wasm_bindgen]
 pub struct Core {
     world: World,
@@ -20,7 +35,13 @@ impl Core {
         Self { world, schedule }
     }
 
-    pub fn update(&mut self, _delta: f64) {
+    pub fn update(&mut self, timestamp: f64) {
+        let mut update_interval = self.world.get_resource_or_insert_with(|| UpdateInterval {
+            timestamp,
+            delta: 0.0,
+        });
+        update_interval.delta = timestamp - update_interval.timestamp;
+        update_interval.timestamp = timestamp;
         self.schedule.run(&mut self.world);
     }
 }
