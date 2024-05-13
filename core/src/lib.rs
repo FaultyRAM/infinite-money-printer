e02dcd9e062d98bf09db6c6a87b61bb3cf0056f7
FaultyRAM <316014+FaultyRAM@users.noreply.github.com>
2024-05-13 22:58:33 +1000

    fix(core, frontend): move update callback fully into WASM

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -12,29 +12,34 @@ use crate::{
     ui::update_ui,
 };
 use bevy_ecs::prelude::*;
+use std::{cell::RefCell, rc::Rc};
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub struct GameState {
-    world: World,
-    schedule: Schedule,
+fn request_animation_frame(f: &Closure<dyn FnMut(f64)>) {
+    let window = web_sys::window().unwrap();
+    let _ = window
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .unwrap();
 }
 
-#[wasm_bindgen]
-impl GameState {
-    #[wasm_bindgen(constructor)]
-    pub fn new(timestamp: f64) -> Self {
-        let mut world = World::new();
-        let mut schedule = Schedule::default();
-        world.insert_resource(UpdateInterval::new(timestamp));
-        world.insert_resource(Credits::new());
-        schedule.add_systems((update_credits, update_ui).chain());
-        Self { world, schedule }
-    }
-
-    pub fn update(&mut self, timestamp: f64) {
-        let mut update_interval: Mut<UpdateInterval> = self.world.resource_mut();
+#[wasm_bindgen(start)]
+fn run() {
+    let timestamp = web_sys::window()
+        .and_then(|window| window.document())
+        .and_then(|document| document.timeline().current_time())
+        .unwrap();
+    let mut world = World::new();
+    let mut schedule = Schedule::default();
+    world.insert_resource(UpdateInterval::new(timestamp));
+    world.insert_resource(Credits::new());
+    schedule.add_systems((update_credits, update_ui).chain());
+    let update_cb = Rc::new(RefCell::new(None));
+    let ucb_clone = update_cb.clone();
+    *ucb_clone.borrow_mut() = Some(Closure::new(move |timestamp| {
+        let mut update_interval: Mut<UpdateInterval> = world.resource_mut();
         *update_interval = update_interval.advance(timestamp);
-        self.schedule.run(&mut self.world);
-    }
+        schedule.run(&mut world);
+        request_animation_frame(update_cb.borrow().as_ref().unwrap());
+    }));
+    request_animation_frame(ucb_clone.borrow().as_ref().unwrap());
 }
